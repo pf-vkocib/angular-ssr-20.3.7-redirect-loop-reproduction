316b626e12b1299ff85fe59cd53aecc6b1e1d69c
Vladimír Kočib <vladimir.kocib@papirfly.com>
2025-11-20 15:53:59 +0100

    ssr redirect loop minimal reproduction

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+  REQUEST_CONTEXT,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    {
+      provide: APP_BASE_HREF,
+      useFactory: (ctx = inject<{ customPath?: string }>(REQUEST_CONTEXT)) => {
+        return ctx?.customPath ?? '';
+      },
+    },
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<p>app</p>`,
 })
 export class App {
-  protected readonly title = signal('ssr-redirect-loop');
+  constructor() {
+    console.log('app comp init');
+  }
 }
